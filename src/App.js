b7d33e8ad18a2ba3427389780b6f6f3a543212b3
Gustav Burchardt <gustavburchardt@gmail.com>
2020-02-27 15:22:25 +0100

    Add material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import './App.css';
+import {
+  AppBar, Toolbar, IconButton, Typography, Button
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}))
 
 function App() {
+  const classes = useStyles()
   return (
-    <div className="App">
+    <div className={classes.root}>
+      <AppBar>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            App
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
