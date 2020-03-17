5cc2ad5d5413e16d452491c143be4fa1898a120e
Gustav Burchardt <gustavburchardt@gmail.com>
2020-03-17 16:24:07 +0100

    Rewrite to class type component

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,15 +1,6 @@
-import React, { useMemo, useEffect } from 'react'
+import React, { Component } from 'react'
 import { makeStyles } from '@material-ui/styles'
-import { useSelector } from 'react-redux'
-
-const sortByIndex = (a, b) => {
-  if (a.index < b.index) {
-    return -1
-  } else if (a.index > b.index) {
-    return 1
-  }
-  return 0
-}
+import { connect } from 'react-redux'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -26,37 +17,13 @@ const useStyles = makeStyles(theme => ({
 
 const contexts = {}
 
-const Canvas = () => {
+const CanvasRenderer = React.forwardRef((props, ref) => {
   const classes = useStyles()
-
-  const {
-    layers
-  } = useSelector(state => state.canvas)
-
-  const layersOrderIdentity = layers.map(layer => layer.id).join('')
-  const layersTimeIdentity = layers.map(layer => layer.timestamp).join('')
-
-  const layersSorted = useMemo(() =>
-    [...layers].sort(sortByIndex),
-    // eslint-disable-next-line
-    [layersOrderIdentity]
-  )
-
-  useEffect(
-    () => {
-      layers.forEach(layer => {
-        const ctx = contexts[layer.id]
-
-        ctx.drawImage(layer.canvas, 0, 0)
-      })
-    },
-    // eslint-disable-next-line
-    [layersTimeIdentity]
-  )
+  const { layers } = props
 
   return (
-    <div className={classes.root}>
-      {layersSorted.map(layer => (
+    <div className={classes.root} ref={ref}>
+      {layers.map(layer => (
         <canvas
           key={layer.id}
           width={layer.width}
@@ -67,6 +34,85 @@ const Canvas = () => {
       ))}
     </div>
   )
+})
+
+class CanvasContainer extends Component {
+  constructor (props) {
+    super(props)
+
+    this._mouseDown = this.mouseDown.bind(this)
+    this._mouseMove = this.mouseMove.bind(this)
+    this._mouseUp = this.mouseUp.bind(this)
+
+    this.element = React.createRef()
+  }
+
+  componentDidMount () {
+    const node = this.element.current
+    if (node) {
+      node.addEventListener('mousedown', this._mouseDown)
+      node.addEventListener('mousemove', this._mouseMove)
+      node.addEventListener('mouseup', this._mouseUp)
+    }
+
+    this.updateContexts()
+  }
+
+  componentWillUnmount () {
+    const node = this.element.current
+    if (node) {
+      node.removeEventListener('mousedown', this._mouseDown)
+      node.removeEventListener('mousemove', this._mouseMove)
+      node.removeEventListener('mouseup', this._mouseUp)
+    }
+  }
+
+  componentDidUpdate (prevProps) {
+    if (prevProps.canvas.masterTimestamp < this.props.canvas.masterTimestamp) {
+      this.updateContexts(prevProps)
+    }
+  }
+
+  updateContexts (prevProps) {
+    this.props.canvas.layers.forEach(layer => {
+      const ctx = contexts[layer.id]
+
+      if (!prevProps) {
+        ctx.drawImage(layer.canvas, 0, 0)
+      } else {
+        const lastLayer = prevProps.canvas.layers.find(l => l.id === layer.id)
+
+        if (!lastLayer || lastLayer.timestamp < layer.timestamp) {
+          ctx.drawImage(layer.canvas, 0, 0)
+        }
+      }
+    })
+  }
+
+  mouseDown () {
+
+  }
+
+  mouseMove () {
+
+  }
+
+  mouseUp () {
+
+  }
+
+  render () {
+    return (
+      <CanvasRenderer
+        layers={this.props.canvas.layers}
+        ref={this.element}
+      />
+    )
+  }
 }
 
-export default Canvas
+export default connect(
+  state => ({
+    canvas: state.canvas
+  })
+)(CanvasContainer)
--- src/reducers/canvas.js
+++ src/reducers/canvas.js
@@ -2,19 +2,31 @@ import { Layer } from 'models/layer'
 
 const defaultState = {
   currentLayer: -1,
+  masterTimestamp: 0,
+  layerOrder: '',
   layers: []
 }
+
+// const sortByIndex = (a, b) => {
+//   if (a.index < b.index) {
+//     return -1
+//   } else if (a.index > b.index) {
+//     return 1
+//   }
+//   return 0
+// }
 
 function canvasReducer (state = { ...defaultState }, action) {
   switch (action.type) {
     case 'NEW_IMAGE': {
       const { width, height } = action.payload
+      const newLayer = new Layer(width, height, 0)
       return {
         ...state,
         currentLayer: 0,
-        layers: [
-          new Layer(width, height, 0)
-        ]
+        masterTimestamp: Date.now(),
+        layers: [newLayer],
+        layerOrder: newLayer.id
       }
     }
     default:
